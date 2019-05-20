18709f5bf9bbee6f10d656958656963f1507b137
dk <1303997999@qq.com>
2019-05-20 10:56:28 +0800

    fjjfjfjfjj

--- test.java
+++ test.java
@@ -50,3 +50,18 @@ public class Singleton(){
 		return singleton;
 	}
 }
+public class signClss {
+    private static  SignState = null;
+    private signClss(){
+    }
+    public static SignState getSign(){
+        if (SignState==null){
+          synchronized(signClss.class){
+            if (SignState==null){
+                SignState = new SignState();
+             }
+            }
+        }
+    }
+
+}
