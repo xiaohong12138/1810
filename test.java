e1adb3c5d853d785536f34946a28c4921566947f
Boryer <535047778@qq.com>
2019-05-20 10:47:55 +0800

    boryer zhang

--- test.java
+++ test.java
@@ -13,3 +13,20 @@ public class signClss {
     }
 
 }
+
+
+//boryer zhang
+public class Singleton(){
+	private Singleton singleton;
+	private Singleton(){};
+	public Singleton getSingleton(){
+		if(singleton==null){
+			synchronized(Singleton.class){
+				if(singleton==null){
+					singleton = new Singleton£¨£©£»
+				}
+			}
+		}
+		return singleton;
+	}
+}
