39dad9f64f50722c76b9428a22a8e6d4fb4ad6ae
wjbo123 <289608861@qq.com>
2019-05-20 11:08:48 +0800

    yyyy

--- test.java
+++ test.java
@@ -65,3 +65,19 @@ public class signClss {
     }
 
 }
+/*********************/
+public class signClss {
+    private static  SignState = null;
+    private signClss(){
+    }
+    public static SignState getSign(){
+        if (SignState==null){
+          synchronized(signClss.class){
+            if (SignState==null){
+                SignState = new SignState();
+             }
+            }
+        }
+    }
+
+}
