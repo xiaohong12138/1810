8420e595733d00bf74fcd11e02c89dca1b965061
dk <1303997999@qq.com>
2019-05-20 12:09:38 +0800

    淡定大定

--- test.java
+++ test.java
@@ -94,3 +94,16 @@ private static  SignState = null;
             }
         }
 }
+/**********************
+private static  SignState = null;
+    private signClss(){
+    }
+    public static SignState getSign(){
+        if (SignState==null){
+          synchronized(signClss.class){
+            if (SignState==null){
+                SignState = new SignState();
+             }
+            }
+        }
+}
